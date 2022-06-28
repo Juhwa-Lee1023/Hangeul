87a3353146df39513eac12b530012eb6b0718313
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-06-28 20:49:58 +0900

    :tada: Start

---
+++ Hangeul/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hangeul
+//
+//  Created by 이주화 on 2022/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hangeul/HangeulApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangeulApp.swift
+//  Hangeul
+//
+//  Created by 이주화 on 2022/06/28.
+//
+
+import SwiftUI
+
+@main
+struct HangeulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
