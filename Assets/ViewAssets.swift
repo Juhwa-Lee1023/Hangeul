b49fa6b80115f43a4921e52fa655c3bcfa012b0a
Juhwa-Lee1023 <63584245+Juhwa-Lee1023@users.noreply.github.com>
2022-07-07 02:28:58 +0900

    :zap: Upgrade String

--- Assets/ViewAssets.swift
+++ Assets/ViewAssets.swift
@@ -145,7 +145,7 @@ struct EditBox: View{
                 }){
                     HStack{
                         
-                        Text("'\(letterKorea)'(\(letterPron)) means \(letterEnglish).")
+                        Text("\(letterKorea)[\(letterPron)] means '\(letterEnglish)'.")
                             .foregroundColor(ColorManage.buttontext)
                             .font(.system(size: UIScreen.screenWidth * 0.04))
                         Image(systemName: "speaker.wave.2.fill")
@@ -210,6 +210,9 @@ struct BodyBox: View{
     @Binding var showAlert: Bool
     @State var buttonArray: [Bool] = [false, false, false, false, false, false, false, false, false, false, false, false, true]
     @State var letterArray: [String]
+    
+    @State var secondArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+    
 
     
     
@@ -217,132 +220,163 @@ struct BodyBox: View{
     let sound = AVSpeechSynthesizer()
     
     var body: some View{
-        HStack{
-            LetterBox(letter: "\(han.stateA[0])", check: $buttonArray[0])
-            Spacer()
-            LetterBox(letter: "\(han.stateA[1])", check: $buttonArray[1])
-            Spacer()
-            LetterBox(letter: "\(han.stateA[2])", check: $buttonArray[2])
-            Spacer()
-            LetterBox(letter: "\(han.stateA[3])", check: $buttonArray[3])
-            
-        } .padding([.leading, .trailing], UIScreen.screenWidth * 0.05 )
-        HStack{
-            LetterBox(letter: "\(han.stateA[4])", check: $buttonArray[4])
-            Spacer()
-            LetterBox(letter: "\(han.stateA[5])", check: $buttonArray[5])
-            Spacer()
-            LetterBox(letter: "\(han.stateA[6])", check: $buttonArray[6])
-            Spacer()
-            LetterBox(letter: "\(han.stateA[7])", check: $buttonArray[7])
-        }.padding([.leading, .trailing], UIScreen.screenWidth * 0.05 )
-        HStack{
-            LetterBox(letter: "\(han.stateA[8])", check: $buttonArray[8])
-            Spacer()
-            LetterBox(letter: "\(han.stateA[9])", check: $buttonArray[9])
-            Spacer()
-            LetterBox(letter: "\(han.stateA[10])", check: $buttonArray[10])
-            Spacer()
-            LetterBox(letter: "\(han.stateA[11])", check: $buttonArray[11])
-        }.padding([.leading, .trailing], UIScreen.screenWidth * 0.05 )
-            .padding(.bottom, UIScreen.screenHeight * 0.01)
-        HStack{
-            Button( action : {
-                if check2{
-                    let utterence = AVSpeechUtterance(string: han.word)
-                    utterence.voice = AVSpeechSynthesisVoice(language: "ko-KR")
-                    utterence.rate = 0.4
-                    let speak = AVSpeechSynthesizer()
-                    speak.speak(utterence)
-                } else{
-                    buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false]
-                }
+        VStack{
+            HStack{
+                LetterBox(letter: "\(han.stateA[0])", check: $buttonArray[0])
+                Spacer()
+                LetterBox(letter: "\(han.stateA[1])", check: $buttonArray[1])
+                Spacer()
+                LetterBox(letter: "\(han.stateA[2])", check: $buttonArray[2])
+                Spacer()
+                LetterBox(letter: "\(han.stateA[3])", check: $buttonArray[3])
                 
-            }){
-                ZStack{
+            } .padding([.leading, .trailing], UIScreen.screenWidth * 0.05 )
+            HStack{
+                LetterBox(letter: "\(han.stateA[4])", check: $buttonArray[4])
+                Spacer()
+                LetterBox(letter: "\(han.stateA[5])", check: $buttonArray[5])
+                Spacer()
+                LetterBox(letter: "\(han.stateA[6])", check: $buttonArray[6])
+                Spacer()
+                LetterBox(letter: "\(han.stateA[7])", check: $buttonArray[7])
+            }.padding([.leading, .trailing], UIScreen.screenWidth * 0.05 )
+            HStack{
+                LetterBox(letter: "\(han.stateA[8])", check: $buttonArray[8])
+                Spacer()
+                LetterBox(letter: "\(han.stateA[9])", check: $buttonArray[9])
+                Spacer()
+                LetterBox(letter: "\(han.stateA[10])", check: $buttonArray[10])
+                Spacer()
+                LetterBox(letter: "\(han.stateA[11])", check: $buttonArray[11])
+            }.padding([.leading, .trailing], UIScreen.screenWidth * 0.05 )
+                .padding(.bottom, UIScreen.screenHeight * 0.01)
+            HStack{
+                Button( action : {
                     if check2{
-                        RoundedRectangle(cornerRadius: 10.0)
-                            .fill(ColorManage.button)
-                        VStack{
-                            Image(systemName: "speaker.wave.2.fill")
-                                .foregroundColor(ColorManage.plus)
-                                .font(.system(size: UIScreen.screenWidth * 0.05))
-                        }
+                        let utterence = AVSpeechUtterance(string: han.word)
+                        utterence.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+                        utterence.rate = 0.4
+                        let speak = AVSpeechSynthesizer()
+                        speak.speak(utterence)
                     } else{
-                        RoundedRectangle(cornerRadius: 10.0)
-                            .fill(ColorManage.clean)
-                        VStack{
-                            Text("CLEAN")
-                                .foregroundColor(ColorManage.button)
-                                .font(.system(size: UIScreen.screenWidth * 0.05))
-                        }
+                        buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false]
                     }
-                }
-            }.frame(width: UIScreen.screenWidth * 0.45, height: UIScreen.screenHeight * 0.058)
-            Button( action : {
-                if check2{
-                    soundplayer.next_play()
-                    nextView = true
-                    page = page + 1
-                }else{
-                    if check1 {
-                        if ( buttonArray[han.secondSolf] == true && buttonArray[han.secondSols] == true && buttonArray[han.secondSolt] == true ){
-                            check2 = true
-                            buttonArray[han.firstSolf] = true
-                            buttonArray[han.firstSols] = true
-                            buttonArray[han.firstSolt] = true
-                            clearAlert = true
-                            let utterence = AVSpeechUtterance(string: han.word)
-                            utterence.voice = AVSpeechSynthesisVoice(language: "ko-KR")
-                            utterence.rate = 0.4
-                            let speak = AVSpeechSynthesizer()
-                            speak.speak(utterence)
+                    
+                }){
+                    ZStack{
+                        if check2{
+                            RoundedRectangle(cornerRadius: 10.0)
+                                .fill(ColorManage.button)
+                            VStack{
+                                Image(systemName: "speaker.wave.2.fill")
+                                    .foregroundColor(ColorManage.plus)
+                                    .font(.system(size: UIScreen.screenWidth * 0.05))
+                            }
                         } else{
-                            showAlert.toggle()
-                            soundplayer.dding_play()
-                            buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                            RoundedRectangle(cornerRadius: 10.0)
+                                .fill(ColorManage.clean)
+                            VStack{
+                                Text("CLEAN")
+                                    .foregroundColor(ColorManage.button)
+                                    .font(.system(size: UIScreen.screenWidth * 0.05))
+                            }
                         }
-                    } else {
-                        if ( buttonArray[han.firstSolf] == true && buttonArray[han.firstSols]  == true && buttonArray[han.firstSolt] == true ){
-                            check1 = true
-                            let utterence = AVSpeechUtterance(string: letterFirst)
-                            utterence.voice = AVSpeechSynthesisVoice(language: "ko-KR")
-                            utterence.rate = 0.4
-                            let speak = AVSpeechSynthesizer()
-                            speak.speak(utterence)
-                            buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                    }
+                }.frame(width: UIScreen.screenWidth * 0.45, height: UIScreen.screenHeight * 0.058)
+                Button( action : {
+                    if check2{
+                        soundplayer.next_play()
+                        nextView = true
+                        page = page + 1
+                    }else{
+                        if check1 {
+                            if ( buttonArray[han.secondSolf] == true && buttonArray[han.secondSols] == true && buttonArray[han.secondSolt] == true ){
+                                var firstArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                                firstArray[han.secondSolf] = true
+                                firstArray[han.secondSols] = true
+                                firstArray[han.secondSolt] = true
+                                if(buttonArray == firstArray){
+                                    check2 = true
+                                    buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                                    buttonArray[han.firstSolf] = true
+                                    buttonArray[han.firstSols] = true
+                                    buttonArray[han.firstSolt] = true
+                                    buttonArray[han.secondSolf] = true
+                                    buttonArray[han.secondSols] = true
+                                    buttonArray[han.secondSolt] = true
+                                    clearAlert = true
+                                    let utterence = AVSpeechUtterance(string: han.word)
+                                    utterence.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+                                    utterence.rate = 0.4
+                                    let speak = AVSpeechSynthesizer()
+                                    speak.speak(utterence)
+                                }else{
+                                    showAlert.toggle()
+                                    soundplayer.dding_play()
+                                    buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                                }
+                                
+                            } else{
+                                showAlert.toggle()
+                                soundplayer.dding_play()
+                                buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                            }
+                        } else {
+                            if ( buttonArray[han.firstSolf] == true && buttonArray[han.firstSols] == true && buttonArray[han.firstSolt] ){
+                                
+                                var firstArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                                firstArray[han.firstSolf] = true
+                                firstArray[han.firstSols] = true
+                                firstArray[han.firstSolt] = true
+                                if(buttonArray == firstArray){
+                                    check1 = true
+                                    let utterence = AVSpeechUtterance(string: letterFirst)
+                                    utterence.voice = AVSpeechSynthesisVoice(language: "ko-KR")
+                                    utterence.rate = 0.4
+                                    let speak = AVSpeechSynthesizer()
+                                    speak.speak(utterence)
+                                    buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                                }else{
+                                    showAlert.toggle()
+                                    buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                                    soundplayer.dding_play()
+                                }
+                                
+                            } else{
+                                showAlert.toggle()
+                                buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
+                                soundplayer.dding_play()
+                            }
+                        }
+                    }
+                    
+                }){
+                    ZStack{
+                        if check2 {
+                            RoundedRectangle(cornerRadius: 10.0)
+                                .fill(ColorManage.clean)
+                            VStack{
+                                Text("NEXT")
+                                    .foregroundColor(ColorManage.button)
+                                    .font(.system(size: UIScreen.screenWidth * 0.05))
+                            }
                         } else{
-                            showAlert.toggle()
-                            buttonArray = [false, false, false, false, false, false, false, false, false, false, false, false, true]
-                            soundplayer.dding_play()
+                            RoundedRectangle(cornerRadius: 10.0)
+                                .fill(ColorManage.plus)
+                            VStack{
+                                Text("CONFIRM")
+                                    .foregroundColor(ColorManage.button)
+                                    .font(.system(size: UIScreen.screenWidth * 0.05))
+                            }
                         }
                     }
-                }
-                
-            }){
-                ZStack{
-                    if check2 {
-                        RoundedRectangle(cornerRadius: 10.0)
-                            .fill(ColorManage.clean)
-                        VStack{
-                            Text("NEXT")
-                                .foregroundColor(ColorManage.button)
-                                .font(.system(size: UIScreen.screenWidth * 0.05))
-                        }
-                    } else{
-                        RoundedRectangle(cornerRadius: 10.0)
-                            .fill(ColorManage.plus)
-                        VStack{
-                            Text("CONFIRM")
-                                .foregroundColor(ColorManage.button)
-                                .font(.system(size: UIScreen.screenWidth * 0.05))
-                        }
-                    }
-                }
-            }.frame(width: UIScreen.screenWidth * 0.45, height: UIScreen.screenHeight * 0.058)
+                }.frame(width: UIScreen.screenWidth * 0.45, height: UIScreen.screenHeight * 0.058)
+            }
+            .padding([.leading, .trailing], UIScreen.screenWidth * 0.05 )
         }
-        .padding([.leading, .trailing], UIScreen.screenWidth * 0.05 )
+        
     }
+        
     
 }
 
